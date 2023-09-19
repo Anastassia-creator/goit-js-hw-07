3b7c2ec0c672fcc4eef540db6d09f7a37a05a6b8
Anastassia-creator <118266539+Anastassia-creator@users.noreply.github.com>
2023-09-19 16:42:45 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector('.gallery');
 const galleryLi = galleryItems.map(({preview, original, description}) =>
 `<li data-preview=${preview} class="gallery__item">
@@ -19,7 +17,7 @@ gallery.insertAdjacentHTML("beforeend", galleryLi);
 gallery.addEventListener("click", onClick)
 
 function onClick (evt) {
-    // evt.preventDefoult();
+    evt.preventDefault();
     const dataPreview = evt.target.dataset.source;
     const img = evt.target.alt;
     const instance = basicLightbox.create(`
